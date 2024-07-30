716356f191baa93b32c4e99814b5056a2e0ed2c1
Tue Ngo <ngodinh.minhtue@gmail.com>
2024-07-30 07:08:59 +0200

    changed dummy data

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-let items = ["buy swim suits", "buy sanity gel", "go to train station", "hop on the train", "get caught by regulator", "pay ten times the original price"];
+let items = ["Get up", "Grap a coffee", "Happy coding !"];
 
 app.get("/api/items", (req, res) => {
   res.json(items);
 });
 
 app.post("/api/items", (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   items.push(req.body.item);
   res.status(201).json(req.body.item);
 });
